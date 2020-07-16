6d0f0319a57a5fe766550981fa46f904e847c35d
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-17 02:54:04 +0300

    styles moved from inline styles to css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
+import "../styles/main.css";
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <style jsx global>{`
-        body {
-          font-family: "Roboto", sans-serif;
-        }
-      `}</style>
     </>
   );
 }
