0440176272a01a3cd0121c2c35b6e154491c714b
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-17 02:50:41 +0300

    global font family added

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          font-family: "Roboto", sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
