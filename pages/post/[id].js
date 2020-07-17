f2969bed28187914dde5f9f57d5e455cefc9c2d8
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-17 12:12:38 +0300

    navigation between pages is ready

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,25 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default function Post() {
+export default function Post({ post }) {
   const router = useRouter();
   return (
     <>
-      <h1>Post {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <Link href="/posts">
+        <a>Back to posts</a>
+      </Link>
     </>
   );
 }
+
+Post.getInitialProps = async (ctx) => {
+  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+  const post = await response.json();
+
+  return {
+    post,
+  };
+};
