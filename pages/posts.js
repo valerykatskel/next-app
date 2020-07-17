ee3145f134e604468f1af619d403aa4b95b2bfb5
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-17 12:00:18 +0300

    list of posts fixed

--- pages/posts.js
+++ pages/posts.js
@@ -1,4 +1,5 @@
 //import { useState, useEffect } from "react";
+import Link from "next/link";
 import { MainLayout } from "../components/MainLayout";
 
 export default function Posts({ posts }) {
@@ -16,7 +17,15 @@ export default function Posts({ posts }) {
   return (
     <MainLayout title="Posts page">
       <h1>Posts page</h1>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   );
 }
