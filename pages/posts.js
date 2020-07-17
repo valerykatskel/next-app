c5eb567cf1d2da93c4b94799e40f8df04fc594b2
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-17 11:53:30 +0300

    ssr added for mock data

--- pages/posts.js
+++ pages/posts.js
@@ -1,10 +1,31 @@
+//import { useState, useEffect } from "react";
 import { MainLayout } from "../components/MainLayout";
 
-export default function Index() {
+export default function Posts({ posts }) {
+  //const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   async function load() {
+  //     const response = await fetch("http://localhost:4200/posts");
+  //     const posts = await response.json();
+  //     setPosts(posts);
+  //   }
+  //   load();
+  // }, []);
+
   return (
     <MainLayout title="Posts page">
       <h1>Posts page</h1>
-      <p>page description</p>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </MainLayout>
   );
 }
+
+Posts.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/posts");
+  const posts = await response.json();
+
+  return {
+    posts,
+  };
+};
